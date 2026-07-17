1166f8eaa478388fbe7b26cfc80366faba2fe8ac
Jason Christopherson <jchristopherson@hotmail.com>
2026-07-16 21:38:15 -0500

    Fix dependency issue

--- configure/template.cmake
+++ configure/template.cmake
@@ -2,12 +2,9 @@
 
 include(CMakeFindDependencyMacro)
 find_dependency(OpenMP COMPONENTS Fortran)
-
-if (NOT BUILD_SHARED_LIBS)
-  find_dependency(ferror QUIET)
-  find_dependency(linalg QUIET)
-  find_dependency(collections QUIET)
-endif()
+find_dependency(ferror QUIET)
+find_dependency(linalg QUIET)
+find_dependency(collections QUIET)
 
 if(NOT TARGET "@PROJECT_NAME@::@PROJECT_NAME@")
   include("${CMAKE_CURRENT_LIST_DIR}/@PROJECT_NAME@-targets.cmake")
